f9c3c3f6c702a6b6b6ca75c613d76b1fdbb838ea
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-06-22 18:53:36 -0300

    Adicionada rota personalizada para filtrar tarifas por data de vigência

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "tarifas#index"
   resources :empresas, only: [:index, :show]
+  resources :tarifas, only: [:index] do
+    collection do
+      get 'vigenciaempresas/:inicio/:fim', to: 'tarifas#data_vigencia_cnpj'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
