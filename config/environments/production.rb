b403c44a8c03b042463a081b8b35072c81dd785c
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-07-22 22:03:58 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    addresses: 'smtp.gmail.com'
+    addresses: 'smtp.gmail.com',
     port: 587,
     domain: 'dcomp.ufs.br',
     user_name: ENV["DATABASE_USER_NAME"],
