1a1c0f48867c7f5e5d9f44f2e9ad737b2c2584ce
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-07-23 22:41:14 -0300

    Revert "Update cors.rb"
    This reverts commit ec8f6f8a07a9b3a88932da51fcea9f5c506b2230.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*',
-          headers: :any,
-          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          methods: [:get, :post, :options, :delete, :put]
+    resource '*', headers: :any, methods: [:get, :post]
   end
 end
